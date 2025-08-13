729a23138eb69d0ebc9b563c2c27cc325e2516ba
Robert <Roberttovarv@gmail.com>
2025-08-13 17:21:40 +0200

    Adding screens

---
+++ src/screens/MainScreen.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export const MainScreen = () => {
+    return (
+        <View>
+            <Text>Hola mundo</Text>
+        </View>
+    )
+}
---
+++ src/screens/missions list/MissionList.tsx
@@ -0,0 +1,7 @@
+import { FlatList } from "react-native"
+
+export const MissionList = () => {
+    return (
+        <FlatList />
+    )
+}
